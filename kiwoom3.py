b77a782d2eb5ddae568112e32d3bc0bb4db744d7
codebummer <holysmokes@hanmail.net>
2022-03-31 14:21:58 +0900

    Update kiwoom3.py

--- kiwoom3.py
+++ kiwoom3.py
@@ -4,16 +4,25 @@ def db_factory(dbname):
     return sqlite3.connect('D:/myProjects/myKiwoom/'+dbname)
 
 def df_to_db(df, dbname, table):
-    try:
-        db = db_factory(dbname)
+    with db_factory(dbname) as db:
         print('connected to db')
-    except Error as e:
-        print(e)
+
+        try:
+            df.to_sql(table, db, if_exists = 'replace')
+            print('saved requested data in db')
+        except Error as e:
+            print(e)        
+        
+    # try:
+    #     db = db_factory(dbname)
+    #     print('connected to db')
+    # except Error as e:
+    #     print(e)
     
-    try:
-        df.to_sql(table, db, if_exists = 'replace')
-        print('saved requested data in db')
-    except Error as e:
-        print(e)
+    # try:
+    #     df.to_sql(table, db, if_exists = 'replace')
+    #     print('saved requested data in db')
+    # except Error as e:
+    #     print(e)
 
 
