75a8f472ccf7d621b0d6fa6dc59bc020e9059bc2
codebummer <holysmokes@hanmail.net>
2022-03-31 10:15:46 +0900

    Create kiwoom2.py

---
+++ kiwoom2.py
@@ -0,0 +1,112 @@
+from distutils import errors
+from pickle import TRUE
+from kiwoom1 import *
+import pandas as pd
+import time
+
+
+class tr_requests(Kiwoom):
+    def __init__(self):
+        super().__init__()
+        self.set_comm_request_data_signal_slot()
+   
+    def set_input_value(self, id, value):
+        self.dynamicCall('SetInputValue(QString, QString)', id, value)
+    
+    def comm_request_data(self, rqname, trcode, prenext, scrno):
+        self.dynamicCall('CommRqData(QString, QString, int, QString)', rqname, trcode, prenext, scrno)
+        self.data_request_loop = QEventLoop()
+        self.data_request_loop.exec_()
+    
+    def set_comm_request_data_signal_slot(self):
+        self.OnReceiveTrData.connect(self._receive_tr_data)
+    
+    def _receive_tr_data(self, scrno, rqname, trcode, recordname, prenext, datalen, errcode, msg, splm_msg):
+        if prenext == 2:
+            self.remaining_data = True
+        else:
+            self.remaining_data = False
+
+        if trcode == 'opt10081':
+            self.results_df = self._opt_10081(trcode, recordname)
+
+        try:
+            self.data_request_loop.exit()            
+        except AttributeError as e:
+            raise e            
+
+    def get_comm_data_slot(self, trcode, recordname, index, itemname):
+        return self.dynamicCall('GetCommData(QString, QString, int, QString)', trcode, recordname, index, itemname).strip()
+
+    def _get_repeat_cnt(self, trcode, recordname):
+        return self.dynamicCall('GetRepeatCnt(QString, QSTring)', trcode, recordname)
+
+    def get_item_names(self):
+        return ['일자', '시가', '고가', '저가', '현재가', '거래량', '거래대금']
+    
+    def _opt_10081(self, trcode, recordname):
+        _index = self._get_repeat_cnt(trcode, recordname)
+        _itemnames = self.get_item_names()
+        _opt_10081_df = pd.DataFrame([])
+        for item in _itemnames:  
+            results_sub = []
+            for itemnum in range(_index):            
+                # Do not use strip() here as in self.get_comm_data_slot(trcode, recordname, itemnum, item).strip()
+                # because self.get_comm_data_slot(trcode, recordname, itemnum, item) is returned as an object
+                # strip() cannot function on the returned form.
+                # Instead, use strip() directly on the returned value of GetCommData
+                # strip() can function on that form. 
+                results_sub.append(self.get_comm_data_slot(trcode, recordname, itemnum, item)) 
+            _opt_10081_df[item] = results_sub
+        # Convert strings to date(numbers). 
+        # Without converting, the finplot module cannot operate on it to draw candle sticks
+        _opt_10081_df['일자'] = pd.to_datetime(_opt_10081_df['일자'])         
+        _opt_10081_df = _opt_10081_df.set_index('일자')
+        _opt_10081_df = _opt_10081_df[:].astype(int)
+        print(f'Results for requests are as follows:\n', _opt_10081_df)       
+        return _opt_10081_df
+
+TR_REQ_TIME_INTERVAL = 0.2
+
+def inputs(self, id_value_pairs):
+    for id in id_value_pairs:
+        self.set_input_value(id, id_value_pairs[id])
+
+opt_10081_set_inputs = {
+    '종목코드' : '005930',
+    '기준일자' : '20220325',
+    '수정주가구분' : 1
+}
+
+opt_10081_comm_inputs = ['rq_opt10081', 'opt10081', 0, '0001']
+
+def comm_requsts_handler(self, set_inputs, comm_inputs):
+    # perform the first request without checking if remaining_data is true 
+    # since it is not instantiated before the first CommTrData response is received
+    inputs(self, set_inputs)
+    self.comm_request_data(*comm_inputs)
+
+    while self.remaining_data == True:
+        time.sleep(TR_REQ_TIME_INTERVAL)
+        inputs(self, set_inputs)
+        self.comm_request_data(*comm_inputs)
+
+
+
+
+if __name__ == '__main__':
+    transaction_req = tr_requests()
+    comm_requsts_handler(transaction_req, opt_10081_set_inputs, opt_10081_comm_inputs)
+    df_to_db(transaction_req.results_df, 'daily_records.db', 'Daily_Prices')
+    visualize_finplot(transaction_req.results_df)
+    visualize_plotly(transaction_req.results_df)
+    visualize_mplfinance(transaction_req.results_df)
+
+
+
+
+
+    
+    
+
+
